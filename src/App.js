d0f31a162b808fd684750e29033d8f1338793493
jsma3005 <megabmx4477@gmail.com>
2022-03-26 15:01:31 +0600

    PrivateRoute, create todo pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import * as AuthPages from './apps/Auth/pages'
+import * as TodoPages from './apps/Todo/pages'
+import PrivateRoutes from './components/PrivateRoute'
 
 const App = () => {
   return (
     <Routes>
+      <Route element={<PrivateRoutes />}>
+        <Route path="/" element={<TodoPages.List />} />
+        <Route path="/create" element={<TodoPages.Create />} />
+        <Route path="/todo/:id" element={<TodoPages.Page />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
       <Route path='/auth/login' element={<AuthPages.Login />} />
       <Route path='/auth/register' element={<AuthPages.Register />} />
     </Routes>
